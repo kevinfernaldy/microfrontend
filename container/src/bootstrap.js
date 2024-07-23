87f4e74bb9803a55d51b6f40a7f61b11453247ec
Kevin Fernaldy <kevin.fernaldy@gmail.com>
2024-07-23 21:31:40 +0700

    updated auth webpack prod

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -2,5 +2,5 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 
-// To trigger the workflow
+// To trigger the workflow 2
 ReactDOM.render(<App />, document.querySelector('#root'))
